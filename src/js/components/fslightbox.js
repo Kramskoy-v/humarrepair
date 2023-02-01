a6ead8efad1bee4868f63df4a66542d3dc554ab2
Kramskoy-v <kramskoy.va@gmail.com>
2023-02-01 13:02:36 +0300

    Правка Отзывов

--- src/js/components/fslightbox.js
+++ src/js/components/fslightbox.js
@@ -1,21 +1,3 @@
-if (window.innerWidth > 1200) {
-  const reviewsPhone = document.querySelector('.reviews__phone-bg')
-  const galleryItems = document.querySelectorAll('[data-fslightbox="reviews-gallery"]>img')
-
-  addEventListener('click', (e) => {
-    const target = e.target
-    galleryItems.forEach(item => {
-      if (target && target === item) {
-        reviewsPhone.classList.add('_active')
-      }
-      if (target.classList.contains('fslightbox-absoluted') || target.classList.contains(
-        'fslightbox-toolbar-button')) {
-        reviewsPhone.classList.remove('_active')
-      }
-    })
-
-  })
-}
-if (window.innerWidth > 768) {
+if (window.innerWidth > 576) {
   require('fslightbox')
 }
