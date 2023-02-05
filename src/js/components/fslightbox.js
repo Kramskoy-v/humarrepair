4f55bc029eca8dd440c74d89dbd16fe8f2d87728
Kramskoy-v <kramskoy.va@gmail.com>
2023-02-06 00:32:36 +0300

    Правка mobile-menu

--- src/js/components/fslightbox.js
+++ src/js/components/fslightbox.js
@@ -1,3 +1,4 @@
 if (window.innerWidth > 576) {
   require('fslightbox')
 }
+
--- src/js/components/mobileMenu.js
+++ src/js/components/mobileMenu.js
@@ -1,31 +1,26 @@
 // menu mobile
 const hamburger = document.querySelector('.menu-hamburger');
-const closeMenu = document.querySelector('.menu-close');
-const menu = document.querySelector('.menu')
-const mobileWrapper = document.querySelector('.header__mobile-wrapper');
+const header = document.querySelector('.header')
+
 
 hamburger.addEventListener('click', () => {
-  menu.classList.add('_active');
-  mobileWrapper.classList.add('_show');
-  document.body.style.overflow = 'hidden'
+  header.classList.toggle('_menu-active');
+  document.body.style.overflow === '' ? document.body.style.overflow = 'hidden' :
+    document.body.style.overflow = '';
 })
 
-closeMenu.addEventListener('click', () => {
-  menu.classList.remove('_active');
-  mobileWrapper.classList.remove('_show');
-  document.body.style.overflow = ''
-})
 
 // menu sticky
 const logoMini = document.querySelector('.menu__logo-link-mini');
 const socialsMenu = document.querySelector('.menu__socials');
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= 185) {
-    logoMini.classList.add('_visible');
-    socialsMenu.classList.add('_visible');
-  } else {
-    logoMini.classList.remove('_visible');
-    socialsMenu.classList.remove('_visible');
-  }
-});
+if (window.innerWidth > 768) {
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= 185) {
+      logoMini.classList.add('_visible');
+      socialsMenu.classList.add('_visible');
+    } else {
+      logoMini.classList.remove('_visible');
+      socialsMenu.classList.remove('_visible');
+    }
+  });
+}
