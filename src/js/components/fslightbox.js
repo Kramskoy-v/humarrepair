e2e871f52702a0eb869b036afec27fcab038addf
Kramskoy-v <kramskoy.va@gmail.com>
2023-02-08 14:40:45 +0300

    Gallery reviews

--- src/js/components/fslightbox.js
+++ src/js/components/fslightbox.js
@@ -2,3 +2,13 @@ if (window.innerWidth > 576) {
   require('fslightbox')
 }
 
+window.addEventListener('touchstart', (event) => {
+  const swiping = document.querySelector(
+    '.fslightbox-slide-swiping-hoverer.fslightbox-full-dimension.fslightbox-absoluted')
+
+  const target = event.target
+
+  if (target === swiping) {
+    swiping.style.zIndex = '-1'
+  }
+})
